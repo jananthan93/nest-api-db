0fc0603dfad18a0ca6390d15e73d77586502cbc7
jananthan93 <jjananthan93@gmail.com>
2019-10-25 09:52:16 +0530

    post method

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,11 +1,17 @@
 import { Controller, Get, Body, Param, Delete, Put ,Post} from '@nestjs/common';
 import { PostsService } from './posts.service';
 import { PostEntity } from './post.entity';
+import { PostDto } from './post.dto';
+import { UpdateResult, DeleteResult } from 'typeorm';
 
 @Controller('posts')
 export class PostsController {
     constructor(private readonly postsService:PostsService){}
-    
+    @Post()
+    createPost( @Body() postDto:PostDto):Promise<PostEntity>{
+        return this.postsService.createPost(postDto) ;
+    }
+
     @Get()
     async getPosts():Promise<PostEntity[]>{
         return await this.postsService.findPosts() ;
@@ -16,13 +22,13 @@ export class PostsController {
     }
 
 
-    // @Put(':id')
-    // updatePostById(@Param('id')id , @Body() createPostDto:CreatePostDto):string{
-    //     return  `updated id:${id}`;
-    // }
+    @Put(':id')
+    updatePostById(@Param('id')id , @Body() postDto:PostDto):Promise<UpdateResult>{
+        return this.postsService.updatePost(id,postDto);
+    }
 
     @Delete(':id')
-    deletePostById(@Param('id')id) :string{
-        return `deleted id:${id}`;
+    deletePostById(@Param('id')id) :Promise<DeleteResult>{
+        return this.postsService.deletePost(id);
     }
 }
--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -3,6 +3,7 @@ import { PostEntity } from './post.entity';
 import { PostRepository } from './post.repository';
 import { InjectRepository } from '@nestjs/typeorm';
 import { PostDto } from './post.dto';
+import { UpdateResult, DeleteResult } from 'typeorm';
 
 @Injectable({ scope: Scope.REQUEST })
 export class PostsService {
@@ -10,11 +11,20 @@ export class PostsService {
     @InjectRepository(PostEntity)
     private readonly postRepository: PostRepository,
   ) {}
+
+    createPost(postDto): Promise<PostDto>{
+      return this.postRepository.save(postDto);
+    }
     findPosts(): Promise<PostDto[]> {
       return this.postRepository.find();
     }
     findPostById(id):  Promise<PostDto> {
       return this.postRepository.findOne(id);
     }
-    
+    updatePost(id,postDto): Promise<UpdateResult>{
+      return this.postRepository.update(id,postDto);
+    }
+    deletePost(id): Promise<DeleteResult>{
+      return this.postRepository.delete(id);
+    }
 }
